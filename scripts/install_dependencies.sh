9f7088ec807b2aa2ebc21d08b7efad693b32fd1c
goldh0rse <klas.g.holmberg@gmail.com>
2021-12-08 11:15:13 +0100

    Updated main readme & scripts/readme. Added installation script for script requiremenets.

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root, use sudo"
+   exit 1
+fi
+
+if [[ $0 != './install_dependencies.sh' ]] ; then
+  SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+  cd $SCRIPT_DIR
+fi
+
+## Update packages and Upgrade system
+echo '###Updating OS...'
+sudo apt-get update -y
+echo ''
+
+## Python ##
+echo '###Installing Python'
+sudo apt-get install python3 python3-pip3 -y
+echo ''
+
+## Git ##
+echo '###Installing Git..'
+sudo apt-get install git -y
+echo ''
+
+echo '###Cloning Repository...'
+git clone https://github.com/rtklibexplorer/RTKLIB
+echo ''
+
+echo '###Installing RTKLIB console applications...'
+cd RTKLIB/app/consapp/convbin/gcc
+make
+
+if [ $? -eq 0 ] ; then
+  echo 'convbin build successful.'
+  echo ''
+else
+  echo 'convbin build failed. Exiting.'
+  exit -1
+fi
+
+make install
+if [ $? -eq 0 ] ; then
+  echo 'convbin installation successful.'
+  echo ''
+else
+  echo 'convbin installation failed. Exiting.'
+  exit -1
+fi
+make clean
+
+cd ../../rnx2rtkp/gcc
+make
+
+if [ $? -eq 0 ] ; then
+  echo 'rnx2rtkp build successful.'
+  echo ''
+else
+  echo 'rnx2rtkp build failed. Exiting.'
+  exit -1
+fi
+
+make install
+if [ $? -eq 0 ] ; then
+  echo 'rnx2rtkp installation successful.'
+  echo ''
+else
+  echo 'rnx2rtkp installation failed. Exiting.'
+  exit -1
+fi
+echo 'Installation finished.'
+echo ''
+echo '###Cleaning up'
+cd ../../../../..
+echo $PWD
+rm -rf RTKLIB
+echo 'Finished.'
--- scripts/readme.md
+++ scripts/readme.md
@@ -1,6 +1,5 @@
 # Scripts
 
-
 **filter_positions.py**  
 A python script for filtering out positions from post-processed data,  
 with the help of time stamps from the rover.
@@ -13,10 +12,23 @@ Usage:
   * Time stamps should be formatted as `YYYY:MM:DD HH:MM:SS.MS\n`  
   where `MS` stands for milliseconds.
 * Only requires python3 to be installed.
-
-**convert.sh**  - Does not currently produce the best results
+----
+**convert.sh**   
 A scripted file for simplifying conversion of `.ubx` files to  
-`.obs, .nav, .sbas` and also statically post-processes the data to produce a usable `.pos`. 
+`.obs, .nav, .sbas` and also statically post-processes the data to produce a usable `.pos`.
 
 Uses @rtklibexplorer [RTKBLIB](https://github.com/rtklibexplorer/RTKLIB) fork.
 > Convbin & rnx2rtkp needs to be installed and callable as `convbin` & `rnx2rtkp`
+
+Usage:  
+`./convert.sh <BASE.UBX PATH> <BASE.NAV PATH> <ROVER.UBX PATH>`
+
+---
+
+**install_dependencies.sh**  
+Linux based Bash script that installs rtklib and other dependencies for linux operating systems (\*only tested on *ubuntu 20.04lts*).
+
+* git
+* rtklibexplorer/RTKLIB (\*v.demo5, dec 2021)
+  * convbin
+  * rnx2rtkp
